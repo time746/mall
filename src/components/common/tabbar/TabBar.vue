237bca6132aa2e6e5af56e64aa1b650d57ae3ab7
time746 <746774077@qq.com>
2020-07-04 15:52:19 +0800

    init project

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.tab-bar {
+  display: flex;
+
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  height: 49px;
+  background-color: #f6f6f6;
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
+}
+
+</style>
---
+++ src/views/MainTabBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main-tab-bar">
+    <tab-bar>
+      <tab-bar-item path="/home">
+        <img src="~assets/img/tabbar/home.svg" alt slot="item-icon" />
+        <img src="~assets/img/tabbar/home_active.svg" alt slot="item-icon-active" />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <img src="~assets/img/tabbar/category.svg" alt slot="item-icon" />
+        <img src="~assets/img/tabbar/category_active.svg" alt slot="item-icon-active" />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img src="~assets/img/tabbar/cart.svg" alt slot="item-icon" />
+        <img src="~assets/img/tabbar/cart_active.svg" alt slot="item-icon-active" />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img src="~assets/img/tabbar/profile.svg" alt slot="item-icon" />
+        <img src="~assets/img/tabbar/profile_active.svg" alt slot="item-icon-active" />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "components/common/tabbar/TabBar";
+import TabBarItem from "components/common/tabbar/TabBarItem";
+
+export default {
+  name: 'MainTabBar',
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
